4c32079daeff601c5eac3c605fb24abffbae3d49
LovingDream1 <139085974+LovingDream1@users.noreply.github.com>
2023-09-03 00:50:38 +0800

    Add files via upload

---
+++ Templates/MoveTheArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#define SPEEDUP                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+using namespace std;
+using ll=long long;
+void solve()
+{
+
+}
+
+int main()
+{	
+	SPEEDUP
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+}
