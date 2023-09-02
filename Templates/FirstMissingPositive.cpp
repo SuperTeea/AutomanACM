e4fa3bd83b02c25ea4b609cf0284af7b90400cb8
LovingDream1 <139085974+LovingDream1@users.noreply.github.com>
2023-09-03 00:46:23 +0800

    Add files via upload

---
+++ Templates/FirstMissingPositive.cpp
@@ -0,0 +1,21 @@
+//此模板是为了寻找整个数列中未出现的最小正整数
+int firstMissingPositive(vector<int> nums) {
+        int n = nums.size();
+        for (int& num: nums) {
+            if (num <= 0) {
+                num = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n+1 ;
+    }
